e63ac97daeed56edc47d9ac58973026e470e398d
DulajMaya <m.ranga.md@gmail.com>
2025-01-15 15:28:13 +0530

    skip other browser for jenkin

--- Assignment/src/test/java/com/example/tests/BaseTest.java
+++ Assignment/src/test/java/com/example/tests/BaseTest.java
@@ -2,6 +2,7 @@ package com.example.tests;
 
 import com.example.utils.DriverManager;
 import org.openqa.selenium.WebDriver;
+import org.testng.SkipException;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
@@ -9,13 +10,19 @@ import org.testng.annotations.Parameters;
 
 public class BaseTest {
     protected WebDriver driver;
-    // Check if running in Jenkins by looking for JENKINS_HOME environment variable
-    private static final boolean HEADLESS_MODE = System.getenv("JENKINS_HOME") != null;
+    private static final boolean IS_JENKINS = System.getenv("JENKINS_HOME") != null;
+    private static final boolean HEADLESS_MODE = IS_JENKINS;
 
     @Parameters("browser")
     @BeforeMethod
     public void setUp(@Optional("chrome") String browser) {
+        if (IS_JENKINS && !browser.equalsIgnoreCase("chrome")) {
+            throw new SkipException("Skipping test for " + browser + " browser in Jenkins environment");
+        }
+
         System.out.println("Running in " + (HEADLESS_MODE ? "headless" : "normal") + " mode");
+        System.out.println("Browser: " + browser);
+
         driver = DriverManager.getDriver(browser, HEADLESS_MODE);
     }
 
